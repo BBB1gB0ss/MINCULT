63596bb951e4b21bee8ddfadcdfc68b053ecdcde
BBB1gB0ss <marcosb3b3@gmail.com>
2025-10-08 13:01:06 -0400

    update: corrigiendo estilos

--- Scripts.js
+++ Scripts.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btnCrear = document.getElementById("CrearCuenta");
   const btnLogin = document.getElementById("IniciarSesion");
+  const btnEnviar = document.getElementById("Enviar");
   const formulario = document.getElementById("formulario");
   const login = document.getElementById("login");
   const btnCancelar = document.getElementById("cancelar");
+  const mapcontainer = document.getElementById("mapcontainer");
 
   btnCrear.addEventListener("click", () => {
     formulario.style.display = "flex";
@@ -13,5 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
   btnCancelar.addEventListener("click", () => {
     formulario.style.display = "none";
     login.style.display = "flex";
+  });
+  btnEnviar.addEventListener("click", (e) => {
+    formulario.style.display = "none";
+    mapcontainer.style.display = "block";
+  });
+  btnLogin.addEventListener("click", () => {
+    login.style.display = "none";
+    mapcontainer.style.display = "block";
   });
 });
